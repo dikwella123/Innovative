5c9624ed82acdd5e161e65f3402f69b2541c0f38
dikwella123 <dikwella.perez2@gmail.com>
2017-08-15 09:53:33 +0300

    added html code

--- perezxproject1.py
+++ perezxproject1.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/Login')
+def Login():
+    return render_template('Login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
