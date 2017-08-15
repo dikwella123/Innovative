64c21d5a8c287d971d8281e82e95c6361d0264c2
dikwella123 <dikwella.perez2@gmail.com>
2017-08-15 11:46:56 +0300

    updated links

--- perezxproject1.py
+++ perezxproject1.py
@@ -7,9 +7,29 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/Login')
+@app.route('/login')
 def Login():
     return render_template('Login.html')
+
+@app.route('/register')
+def Register():
+    return render_template('register.html')
+
+@app.route('/products')
+def Products():
+    return render_template('products.html')
+
+@app.route('/profile')
+def Profile():
+    return render_template('profile.html')
+
+@app.route('/about')
+def About():
+    return render_template('about.html')
+
+@app.route('/contact')
+def Contact():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
